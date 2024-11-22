9d5bdfcfd80520fe6997ea54f62d3fba2d13fb71
joegeneq <joegeneq@users.noreply.github.com>
2024-11-22 18:23:36 +0800

    Car props

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Car from './car.js';
+
+function Car(props) {
+  return <h2>I am a { props.brand }!</h2>;
+}
+
+function Garage() {
+  const carName = "Ford";
+  return (
+    <>
+	    <h1>Who lives in my garage?</h1>
+	    <Car brand={ carName } />
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Car />);
-
+root.render(<Garage />);
