bd7d4f46b888ee8a3f9d01e017a24dd0ef17c1f8
joegeneq <joegeneq@users.noreply.github.com>
2024-11-22 18:12:51 +0800

    update

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Car from './Car.js';
+import Car from './car.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Car />);
